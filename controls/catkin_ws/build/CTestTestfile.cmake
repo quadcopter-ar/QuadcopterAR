20dff582244c1f40c5cf15b43355c2aefc4d092b
Gabriel Simmons <simmons.gabe@gmail.com>
2019-02-20 17:32:29 -0800

    remove old workspace

--- controls/catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dronear/QuadcopterAR/controls/catkin_ws/src
-# Build directory: /home/dronear/QuadcopterAR/controls/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(ardupilot_gazebo)
