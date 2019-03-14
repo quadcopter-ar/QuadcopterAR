b05a41bd13d41a87ead10c05283d78f1618ba40c
Gabriel Simmons <simmons.gabe@gmail.com>
2019-03-13 17:13:03 -0700

    ignore ros_ws/build/

--- controls/ros_ws/build/ardupilot_gazebo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dronear/QuadcopterAR/controls/ros_ws/src/ardupilot_gazebo
-# Build directory: /home/dronear/QuadcopterAR/controls/ros_ws/build/ardupilot_gazebo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
